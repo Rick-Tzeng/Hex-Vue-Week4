6377b5c9f6a28ba9c689980ae5813d4d14e97622
Rick Tzeng <rick917jp6@gmail.com>
2022-01-28 15:24:31 +0800

    Week 3 assignment

--- js/api.js
+++ js/api.js
@@ -1,120 +1,154 @@
-// API 站點
-const url = "https://vue3-course-api.hexschool.io/v2";
-// API Path
-const path = "rick917jp6-api";
+// ESM 具名匯出 createApp 方法
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js";
 
-// 取得本機端 cookie 儲存的 Token
-// 並將它加到發送 Request 的 headers 中
-const token = document.cookie.replace(
-  /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
-  "$1"
-);
-axios.defaults.headers.common["Authorization"] = token;
+// 宣告要使用的 Modal
+let addProductModal = null;
+let editProductModal = null;
+let delProductModal = null;
 
-const app = {
+createApp({
   data() {
     return {
-      labels: ["產品名稱", "原價", "售價", "是否啟用", "查看細節"],
-      // 利用 v-model 將新增產品的資料加入到 temp.data 裡
-      temp: {
-        data: {
-          title: "",
-          category: "",
-          origin_price: 0,
-          price: 0,
-          unit: "",
-          description: "",
-          content: "",
-          is_enabled: false,
-          imageUrl: "",
-        },
+      // 遠端 API 站點 & 我的 API Path
+      apiUrl: "https://vue3-course-api.hexschool.io/v2",
+      apiPath: "rick917jp6-api",
+
+      labels: ["分類", "產品名稱", "原價", "售價", "是否啟用", "編輯產品"],
+
+      // 利用 v-model 將新增產品的資料加入到 tempProduct 裡
+      tempProduct: {
+        imagesUrl: [],
       },
-      selectedProduct: {},
       products: [],
     };
   },
+  // 生命週期： Vue 初始化掛載完成
+  mounted() {
+    // 初始化要使用的 Modal
+    addProductModal = new bootstrap.Modal(
+      document.getElementById("addProductModal"),
+      { keyboard: false }
+    );
+    editProductModal = new bootstrap.Modal(
+      document.getElementById("editProductModal"),
+      { keyboard: false }
+    );
+    delProductModal = new bootstrap.Modal(
+      document.getElementById("delProductModal"),
+      { keyboard: false }
+    );
+    // 取得本機端 cookie 儲存的 Token, 並將它加到 axios 發送 Request 的 headers 中
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
+    axios.defaults.headers.common["Authorization"] = token;
+
+    this.checkLogin();
+  },
   methods: {
-    // 確認使用者帳號登入狀態
+    // 確認使用者帳號的登入狀態, 是否還在 token 的有效期限內
     checkLogin() {
-      // 取得 Token（Token 僅需要設定一次）
-      const token = document.cookie.replace(
-        /(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/,
-        "$1"
-      );
-      axios.defaults.headers.common["Authorization"] = token;
+      const apiPath = `${this.apiUrl}/api/user/check`;
       axios
-        .post(`${url}/api/user/check`)
+        .post(apiPath)
         .then((res) => {
-          alert("目前使用者登入中");
+          this.getAllProducts();
         })
         .catch((err) => {
-          alert("沒登錄帳號, 請重新登入");
+          alert("Token 已失效, 請重新登入", err.response.data.message);
           window.location.assign("login.html");
         });
     },
-    // 取得後臺所有產品列表
-    getProducts() {
+
+    // 取得儲存在遠端站點所有的產品列表
+    getAllProducts() {
+      const apiPath = `${this.apiUrl}/api/${this.apiPath}/admin/products`;
+
       axios
-        .get(`${url}/api/${path}/admin/products`)
+        .get(apiPath)
         .then((res) => {
           this.products = res.data.products;
-          if (!this.products.length) {
-            alert("目前沒有任何產品資料, 請新增產品資料");
-          } else {
-            alert("已更新產品資料");
-          }
-        })
-        .catch((err) => {
-          if (window.location.pathname == "/login.html") {
-          } else alert("目前沒有任何產品資料, 請新增產品資料");
-        });
-    },
-    // 新增一個產品
-    addProduct() {
-      axios
-        .post(`${url}/api/${path}/admin/product`, this.temp)
-        .then((res) => {
-          alert("成功新增產品");
-          this.temp = {
-            data: {
-              title: "",
-              category: "",
-              origin_price: 0,
-              price: 0,
-              unit: "",
-              description: "",
-              content: "",
-              is_enabled: false,
-              imageUrl: "",
-            },
-          };
-          this.selectedProduct = {};
-          this.getProducts();
         })
         .catch((err) => {
           alert(err.response.data.message);
         });
     },
-    // 刪除一個產品
-    removeProduct() {
-      const id = this.selectedProduct.id;
-      axios
-        .delete(`${url}/api/${path}/admin/product/${id}`)
-        .then((res) => {
-          alert("成功刪除產品");
-          this.selectedProduct = {};
-          this.getProducts();
-        })
-        .catch((err) => {
-          alert(err.response.data.message);
-        });
-    },
-  },
-  // 生命週期（函式）
-  created() {
-    alert("登入成功");
-    this.getProducts();
-  },
-};
 
-Vue.createApp(app).mount("#app");
+    // 利用 whichModal 的變數來判斷觸發打開哪一個 Modal
+    openModal(whichModal, item) {
+      // 新增
+      if (whichModal === "add") {
+        this.tempProduct = {
+          imagesUrl: [],
+        };
+        addProductModal.show();
+      }
+      // 編輯
+      else if (whichModal === "edit") {
+        // 利用展開的方式淺層複製選定的產品, 避免原來的資料一併被改動
+        this.tempProduct = { ...item };
+        editProductModal.show();
+      }
+      // 刪除
+      else if (whichModal === "delete") {
+        this.tempProduct = { ...item };
+        delProductModal.show();
+      }
+    },
+
+    // 新增產品
+    addProduct() {
+      const apiPath = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+
+      axios
+        .post(apiPath, { data: this.tempProduct })
+        .then((res) => {
+          alert(res.data.message);
+          addProductModal.hide();
+          this.getAllProducts();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+
+    // 編輯產品
+    editProduct() {
+      const apiPath = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+
+      axios
+        .put(apiPath, { data: this.tempProduct })
+        .then((res) => {
+          alert(res.data.message);
+          editProductModal.hide();
+          this.getAllProducts();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+
+    // 刪除產品
+    delProduct() {
+      const apiPath = `${this.apiUrl}/api/${this.apiPath}/admin/product/${this.tempProduct.id}`;
+
+      axios
+        .delete(apiPath)
+        .then((res) => {
+          alert(res.data.message);
+          delProductModal.hide();
+          this.getAllProducts();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        });
+    },
+
+    // 新增更多圖片
+    createImages() {
+      this.tempProduct.imagesUrl = [];
+      this.tempProduct.imagesUrl.push("");
+    },
+  },
+}).mount("#app");
--- js/login.js
+++ js/login.js
@@ -1,9 +1,11 @@
-// API 站點
-const url = "https://vue3-course-api.hexschool.io/v2";
+// ESM 具名匯出 createApp 方法
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js";
 
-const app = {
+createApp({
   data() {
     return {
+      // API 站點
+      apiUrl: "https://vue3-course-api.hexschool.io/v2",
       // 利用 v-model 將使用者輸入 input 的資料雙向綁定在 user 裡面
       user: {
         username: "",
@@ -12,14 +14,13 @@ const app = {
     };
   },
   methods: {
-    // 使用者登入
     login() {
-      // 發送 POST 要求 API 至遠端伺服器驗證帳號
+      // 使用者登入, 發送 POST 要求 API 至遠端伺服器驗證帳號
+      const loginApi = `${this.apiUrl}/admin/signin`;
       axios
-        .post(`${url}/admin/signin`, this.user)
+        .post(loginApi, this.user)
         .then((res) => {
-          // 取得 token 將它寫入本機端的 cookie 中
-          // expires 則是 token 的有效期限
+          // 取得 token 將它寫入本機端的 cookie 中, expires 則是 token 的有效期限
           const { token, expired } = res.data;
           document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
           window.location.assign("products.html");
@@ -31,6 +32,4 @@ const app = {
         });
     },
   },
-};
-
-Vue.createApp(app).mount("#app");
+}).mount("#app");
